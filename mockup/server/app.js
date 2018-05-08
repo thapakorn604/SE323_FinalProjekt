fd2c624fa9f3eca10f9861c93e831b35094489fd
Thapakorn Tuwaemuesa <thapakorn604@gmail.com>
2018-05-08 13:39:40 +0700

    update DB

--- mockup/server/app.js
+++ mockup/server/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const db = require('./db')
 
 // Server static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-});
+app.get('/student/index', (req, res) => {
+  res.json(db)
+})
 
 module.exports = app;
