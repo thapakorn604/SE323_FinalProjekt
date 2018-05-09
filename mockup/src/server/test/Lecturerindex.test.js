e30667e3e356d26e038941b6ba2c7a1a8a3c24f8
qliphothparadox <de_wang@cmu.ac.th>
2018-05-09 10:26:49 +0700

    install jest cli

---
+++ mockup/src/server/test/Lecturerindex.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('../app')
+
+describe('Test the lecturer path', () => {
+    test(' response the GET method and get lecturer array', (done) => {
+        return request(app).get('/lecturer/index').expect(200).then(response =>{
+        	expect(response.body).toBeInstanceOf(Array);
+        })
+    });
+    test(' response the GET by id and get lecturer object', (done) => {
+        return request(app).get('/lecturer/index/101').expect(200).then(response =>{
+        	expect(response.body).toBeInstanceOf(Object);
+        })
+    });
+})
