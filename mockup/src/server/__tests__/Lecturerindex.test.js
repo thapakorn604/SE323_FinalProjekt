762a681581ade09af4744ebb3c26deb34e86cb43
Thapakorn Tuwaemuesa <thapakorn604@gmail.com>
2018-05-09 10:32:42 +0700

    fix test error

--- mockup/src/server/__tests__/Lecturerindex.test.js
+++ mockup/src/server/__tests__/Lecturerindex.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app.js')
+const app = require('../app')
 
 describe('Test the lecturer path', () => {
     test(' response the GET method and get lecturer array', (done) => {
